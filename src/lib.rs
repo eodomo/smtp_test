382b0ab7d5885861ee5db4ab2529cbf4b374b11c
Eric Olerud <eric@olerud.com>
2024-07-17 17:07:54 -0400

    start building lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
+use lettre::Message;
+
+pub struct Email<'A> {
+    from: &'A str,
+    reply_to: &'A str,
+    to: &'A str,
+    subject: &'A str,
+    body: &'A str,
+}
 pub fn build_email(
     from: &str,
     reply_to: &str,
     to: &str,
     subject: &str,
     body: &str,
-) -> lettre::Message::MessageBuilder {
-    Message::builder()
+) -> Message::MessageBuilder {
+    let result = Message::builder()
         .from(from.parse()?)
         .reply_to(reply_to.parse()?)
         .to(to.parse()?)
         .subject(subject.parse()?)
         .body(body.parse()?);
+
+    result
 }
