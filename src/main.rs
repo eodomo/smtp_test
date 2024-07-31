7c142ce931fd40c742cdb6d5375fb5a37061108a
Eric Olerud <eric@olerud.com>
2024-07-31 12:28:06 -0400

    This compiles!... but it panics

--- src/main.rs
+++ src/main.rs
@@ -56,16 +56,16 @@ fn add_arrow_brackets(email_address: &str) -> String {
     format!("<{}>", email_address.trim())
 }
 
-fn get_mx_address(host: &str) -> Result<&Name, io::Error> {
+fn get_mx_address(host: &str) -> Result<Name, io::Error> {
     let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
-    let mx_response = resolver.mx_lookup(host.clone());
+    let mx_response = resolver.mx_lookup(host);
     match mx_response {
         Err(_) => panic!("MX address not found for {}", host),
         Ok(mx_response) => {
             let records = mx_response.iter();
             for record in records {
                 println!("{} {}", record.preference(), record.exchange());
-                return Ok(record.exchange());
+                return Ok(record.exchange().clone());
             }
         }
     }
