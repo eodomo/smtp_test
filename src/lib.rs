d0c951c7ad36de53b6a42c02a08d7fde2258476d
Eric Olerud <eric@olerud.com>
2024-08-16 17:22:38 -0400

    move functions to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,3 +20,51 @@ pub fn get_mx_address(host: &str) -> Result<Name, io::Error> {
     }
     panic!("get_mx_addr match did not complete");
 }
+
+pub fn gather_user_info(
+) -> Result<(String, String, String, String, String), Box<dyn std::error::Error>> {
+    let mut from = String::new();
+    let mut reply_to = String::new();
+    let mut to = String::new();
+    let mut subject = String::new();
+    let mut body = String::new();
+
+    print!("From: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut from)?;
+    print!("Reply To: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut reply_to)?;
+    print!("To: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut to)?;
+    print!("Subject: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut subject)?;
+    print!("Body: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut body)?;
+
+    Ok((from, reply_to, to, subject, body))
+}
+
+pub fn create_email(
+    from: &str,
+    reply_to: &str,
+    to: &str,
+    subject: &str,
+    body: &str,
+) -> Result<Message, Box<dyn std::error::Error>> {
+    let from = add_arrow_brackets(from);
+    let reply_to = add_arrow_brackets(reply_to);
+    let to = add_arrow_brackets(to);
+
+    let email = Message::builder()
+        .from(from.parse()?)
+        .reply_to(reply_to.parse()?)
+        .to(to.parse()?)
+        .subject(subject)
+        .body(body.to_string())?;
+
+    Ok(email)
+}
